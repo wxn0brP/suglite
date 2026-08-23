f5751253684521d89c36c6939393316a201d1c88
wxn0brP <viviennevaltheris@gmail.com>
2026-08-23 22:22:38 +0200

    refactor: split rl

--- src/rl.ts
+++ src/rl.ts
@@ -1,33 +1,16 @@
-import { ChildProcess, spawn, SpawnOptions } from "child_process";
-import { appendFileSync, existsSync, readFileSync, writeFileSync } from "fs";
+import { ChildProcess } from "child_process";
+import { existsSync, writeFileSync } from "fs";
 import Readline from "readline";
 import { configs, mainConfig } from "./config";
-import { COLORS, log, logAdv, LogConfig } from "./logger";
+import { COLORS, log } from "./logger";
 import { killHard, processes } from "./process";
+import { appendHistory, runCustomCommand, uniqueHistory } from "./rl.utils";
+import { multi } from "./multi";
 
 export const customCommandsProcess = new Map<string, ChildProcess>();
 
 if (mainConfig.history && mainConfig.history > 0) {
 	if (!existsSync(".suglite_history")) writeFileSync(".suglite_history", "");
-}
-
-function readHistory() {
-	return existsSync(".suglite_history")
-		? readFileSync(".suglite_history", "utf8").split("\n")
-		: [];
-}
-
-function appendHistory(input: string) {
-	appendFileSync(".suglite_history", input + "\n");
-}
-
-function uniqueHistory(history?: string[]) {
-	if (!history) history = readHistory();
-	history = [
-		...new Set(history),
-	];
-	writeFileSync(".suglite_history", history.join("\n"));
-	return history;
 }
 
 const rlOpts: Readline.ReadLineOptions = {
@@ -193,7 +176,7 @@ export function handleLine(input: string) {
 			}
 			break;
 		case "m":
-			import("./multi").then(({ multi }) => multi());
+			multi();
 			break;
 		case "show-m":
 			log(COLORS.green, "Available multiple configs:");
@@ -250,64 +233,6 @@ export function handleLine(input: string) {
 		log(COLORS.green, "Starting server...");
 		runCustomCommand(cmdTrim, false);
 	}
-}
-
-function logExit(code: number, index?: number) {
-	const cfg: LogConfig = {
-		index,
-	} as any;
-	if (code === 0 || code === null) {
-		cfg.color = COLORS.cyan;
-		cfg.msg = "Majestic exit from custom command.";
-	} else {
-		cfg.color = COLORS.magenta;
-		cfg.msg = `Custom command crashed with exit code ${code}.`;
-	}
-	logAdv(cfg);
-}
-
-function runCustomCommand(
-	command: string,
-	prettyLog: boolean = true,
-	cwd = process.cwd(),
-	index?: number,
-) {
-	const cmdTrim = command.trim();
-	log(COLORS.blue, `Running command: ${command}`);
-
-	const opts: SpawnOptions = {
-		shell: true,
-		cwd,
-	};
-	if (!prettyLog) opts.stdio = "inherit";
-
-	const cmdProcess = spawn(command, opts);
-	customCommandsProcess.set(cmdTrim, cmdProcess);
-
-	if (prettyLog) {
-		cmdProcess.stdout.on("data", data => {
-			logAdv({
-				color: COLORS.cyan,
-				prefix: "[stdout]",
-				msg: data.toString().trim(),
-				index,
-			});
-		});
-		cmdProcess.stderr.on("data", data => {
-			logAdv({
-				color: COLORS.magenta,
-				prefix: "[stderr]",
-				msg: data.toString().trim(),
-				index,
-			});
-		});
-	}
-
-	cmdProcess.on("exit", code => {
-		logExit(code);
-		if (customCommandsProcess.has(cmdTrim))
-			customCommandsProcess.delete(cmdTrim);
-	});
 }
 
 async function exitEvent() {
---
+++ src/rl.utils.ts
@@ -0,0 +1,82 @@
+import { spawn, SpawnOptions } from "child_process";
+import { log } from "console";
+import { appendFileSync, existsSync, readFileSync, writeFileSync } from "fs";
+import { COLORS, logAdv, LogConfig } from "./logger";
+import { customCommandsProcess } from "./rl";
+
+export function runCustomCommand(
+	command: string,
+	prettyLog: boolean = true,
+	cwd = process.cwd(),
+	index?: number,
+) {
+	const cmdTrim = command.trim();
+	log(COLORS.blue, `Running command: ${command}`);
+
+	const opts: SpawnOptions = {
+		shell: true,
+		cwd,
+	};
+	if (!prettyLog) opts.stdio = "inherit";
+
+	const cmdProcess = spawn(command, opts);
+	customCommandsProcess.set(cmdTrim, cmdProcess);
+
+	if (prettyLog) {
+		cmdProcess.stdout.on("data", data => {
+			logAdv({
+				color: COLORS.cyan,
+				prefix: "[stdout]",
+				msg: data.toString().trim(),
+				index,
+			});
+		});
+		cmdProcess.stderr.on("data", data => {
+			logAdv({
+				color: COLORS.magenta,
+				prefix: "[stderr]",
+				msg: data.toString().trim(),
+				index,
+			});
+		});
+	}
+
+	cmdProcess.on("exit", code => {
+		logExit(code);
+		if (customCommandsProcess.has(cmdTrim))
+			customCommandsProcess.delete(cmdTrim);
+	});
+}
+
+function logExit(code: number, index?: number) {
+	const cfg: LogConfig = {
+		index,
+	} as any;
+	if (code === 0 || code === null) {
+		cfg.color = COLORS.cyan;
+		cfg.msg = "Majestic exit from custom command.";
+	} else {
+		cfg.color = COLORS.magenta;
+		cfg.msg = `Custom command crashed with exit code ${code}.`;
+	}
+	logAdv(cfg);
+}
+
+function readHistory() {
+	return existsSync(".suglite_history")
+		? readFileSync(".suglite_history", "utf8").split("\n")
+		: [];
+}
+
+export function appendHistory(input: string) {
+	appendFileSync(".suglite_history", input + "\n");
+}
+
+export function uniqueHistory(history?: string[]) {
+	if (!history) history = readHistory();
+	history = [
+		...new Set(history),
+	];
+	writeFileSync(".suglite_history", history.join("\n"));
+	return history;
+}
